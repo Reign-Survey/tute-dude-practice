e375a11c7346580261b8067f45a396c08f06768d
Kunal <kunalsharmadelhi932@gmail.com>
2025-09-28 20:02:06 +0530

    Tute dude solution

---
+++ Tutedude.py
@@ -0,0 +1,12 @@
+#Task1
+a= int(input("Enter first number: "))
+b= int(input("Enter second number: "))
+print("Sum =",a+b)	#Addition
+print("Subtraction =",a-b)	#Subtraction
+print("Multiplication =",a*b)	#Multiplication
+print("Division of =",a/b)	#Division
+
+#Task2
+Firstname= input("Enter your First name: ")
+Lastname= input("Enter your Last name: ")
+print("Hello "+Lastname+" "+Firstname)
